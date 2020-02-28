03baf7f607a4c3dcc9368cecfb6d57675e60923c
jivanova <Joana.Ivanova@progress.com>
2020-02-28 19:19:19 +0200

    chore: test

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 var seleniumStandalone = require("selenium-standalone");
 var Launcher = require('webdriverio/build/lib/launcher');
 var wdio = new Launcher('./wdioRunner/wdio.conf.js');
-
+//test1
  seleniumStandalone.install(function () {  
     seleniumstandalone.start(function (err, instance) { 
         wdio.run().then(function () {
